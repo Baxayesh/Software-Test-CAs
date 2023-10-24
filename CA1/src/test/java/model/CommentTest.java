4385619d0105d261328e824b0e29f8d3e9952f26
amin <bahonar2002@gmail.com>
2023-10-24 23:14:01 +0330

    Commodity and Comment tests

--- CA1/src/test/java/model/CommentTest.java
+++ CA1/src/test/java/model/CommentTest.java
@@ -1,5 +1,69 @@
 package model;
 
+import exceptions.NotInStock;
+import org.junit.jupiter.api.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
 class CommentTest {
 
+    static Date currentDate ;
+    static Comment comment ;
+    @BeforeAll
+    static void setup(){
+        currentDate = new Date();
+        comment = new Comment(2,"test@test.com","test",32,"good");
+    }
+
+    @DisplayName("Tests Date Comment")
+    @Nested
+    class DateCommentTest{
+        @Test
+        public void GIVEN_Comment_THEN_CommentDateEqualNowDate() {
+            Assertions.assertEquals(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(currentDate), comment.getDate());
+
+        }
+    }
+
+    @DisplayName("Tests Vote Comment")
+    @Nested
+    class VoteCommentTests{
+        @Test
+        public void GIVEN_Comment_WHEN_LikeDuplicateUser_THEN_LikeCountNotChange() {
+            comment.addUserVote("amin" , "like");
+            comment.addUserVote("amin" , "like");
+            Assertions.assertEquals(1, comment.getLike());
+            Assertions.assertEquals(0, comment.getDislike());
+        }
+        @Test
+        public void GIVEN_Comment_WHEN_LikeChangeToDislike_THEN_LikeCountChange() {
+            comment.addUserVote("amin" , "like");
+            comment.addUserVote("amin" , "dislike");
+            Assertions.assertEquals(0, comment.getLike());
+            Assertions.assertEquals(1, comment.getDislike());
+        }
+        @Test
+        public void GIVEN_Comment_WHEN_LikeAndDislikeMultiUser_THEN_CheckLikeAndDislikeCount() {
+            comment.addUserVote("amin" , "like");
+            comment.addUserVote("ali" , "dislike");
+            comment.addUserVote("amir" , "like");
+            Assertions.assertEquals(2, comment.getLike());
+            Assertions.assertEquals(1, comment.getDislike());
+        }
+        @Test
+        public void GIVEN_Comment_WHEN_NotLikeOrDislike_THEN_LikeAndDislikeCountEqualZero() {
+            Assertions.assertEquals(0, comment.getLike());
+            Assertions.assertEquals(0, comment.getDislike());
+        }
+        @Test
+        public void GIVEN_Comment_WHEN_WrongLikeOrDislike_THEN_LikeAndDislikeCountEqualZero() {
+            comment.addUserVote("amin" , "likee");
+            comment.addUserVote("ali" , "dislikee");
+            comment.addUserVote("amir" , "likee");
+            Assertions.assertEquals(0, comment.getLike());
+            Assertions.assertEquals(0, comment.getDislike());
+        }
+    }
 }
--- CA1/src/test/java/model/CommodityTest.java
+++ CA1/src/test/java/model/CommodityTest.java
@@ -1,5 +1,87 @@
 package model;
 
+import exceptions.NotInStock;
+import org.junit.jupiter.api.*;
+
+
 class CommodityTest {
+
+
+    @Nested
+    @DisplayName("Tests updateInStock")
+    class UpdateInStockTests {
+        static Commodity commodity;
+        @BeforeAll
+        static void createAnonymousCommodity(){
+            commodity = new Commodity();
+            commodity.setInStock(5);
+        }
+        @Test
+        public void GIVEN_AnonymousCommodity_WHEN_AddingNegativeInStock_THEN_WillThrowNotInStock() throws NotInStock {
+            var negativeInStock = -10;
+            commodity.updateInStock(negativeInStock);
+            Assertions.assertThrows(
+                    NotInStock.class,
+                    () -> commodity.updateInStock(negativeInStock)
+            );
+        }
+
+        @Test
+        public void GIVEN_AnonymousCommodity_WHEN_AddingZeroInStock_THEN_CommodityInStockWillNotChange()
+                throws NotInStock{
+
+            var zeroIncrement = 0;
+            commodity.updateInStock(zeroIncrement);
+            Assertions.assertEquals(5, commodity.getInStock());
+        }
+
+        @Test
+        public void GIVEN_AnonymousCommodity_WHEN_AddingPositiveInStock_THEN_CommodityInStockWillBeIncreased()
+                throws NotInStock {
+            var positiveIncrement = 10;
+            commodity.updateInStock(positiveIncrement);
+            Assertions.assertEquals(15, commodity.getInStock());
+        }
+    }
+
+    @Nested
+    @DisplayName("Tests For Rating Commodity")
+    public class RatingTests{
+
+        Commodity createAnonymousCommodityInitRate (float initRate){
+            var commodity = new Commodity();
+            commodity.setInitRate(initRate);
+            return commodity;
+        }
+//        Commodity createAnonymousCommodityWithRated(){
+//            var commodity = new Commodity();
+//            return commodity;
+//        }
+
+        @Test
+        public void GIVEN_AnonymousCommodity_WHEN_AddingDuplicateRateToCommodity_THEN_RateShouldChanged(){
+
+            var commodity = createAnonymousCommodityInitRate(5);
+            commodity.addRate("amin" ,10);
+            commodity.addRate("amin" ,3);
+            Assertions.assertEquals(4, commodity.getRating());
+        }
+        @Test
+        public void GIVEN_AnonymousCommodity_WHEN_AddingNotDuplicateRateToCommodity_THEN_RateShouldChanged(){
+
+            var commodity = createAnonymousCommodityInitRate(2);
+            commodity.addRate("amin" ,3);
+            commodity.addRate("ali" ,1);
+            Assertions.assertEquals(2, commodity.getRating());
+        }
+
+//        @Test
+//        public void GIVEN_AnonymousCommodity_WHEN_AddingNegetiveRateToCommodity_THEN_ThrowError(){
+//
+//            var commodity = createAnonymousCommodityInitRate(2);
+//            commodity.addRate("amin" ,-3);
+//            Assertions.assertThrows(, commodity.getRating());
+//        }
+    }
 
 }
