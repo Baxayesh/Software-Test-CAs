fcc5e1e9da35a8a5314278f578ed45af96ad3963
M.R. Bakhshayesh <baxayesh@gmail.com>
2023-11-27 21:58:39 +0330

    chore: adding test groups, display names, etc..

--- CA1/src/test/java/ApiTest/CommoditiesControllerTest.java
+++ CA1/src/test/java/ApiTest/CommoditiesControllerTest.java
@@ -1,6 +1,9 @@
 package ApiTest;
 
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -8,20 +11,34 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import application.BalootApplication;
 import controllers.CommoditiesController;
 import controllers.UserController;
+import exceptions.NotExistentCommodity;
+import exceptions.NotExistentUser;
+import model.Comment;
+import model.Commodity;
 import model.User;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import service.Baloot;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 
 @SpringBootTest(classes = BalootApplication.class)
+@DisplayName("Commodities Controller Api Tests")
 public class CommoditiesControllerTest {
 
     @Autowired
@@ -41,5 +58,41 @@ public class CommoditiesControllerTest {
     }
 
 
+
+    @Nested
+    @DisplayName("Tests For /commodities/{id}/comment")
+    public class CommentTests{
+
+
+
+    }
+
+    @Nested
+    @DisplayName("Tests For /commodities/{id}/rate")
+    public  class RatingTests {
+
+
+    }
+
+    @Nested
+    @DisplayName("Tests For /commodities/search")
+    public  class SearchTests {
+
+
+    }
+
+    @Nested
+    @DisplayName("Tests For /commodities/{id}/suggested")
+    public  class SuggestionTests {
+
+
+    }
+
+    @Nested
+    @DisplayName("Tests For /commodities/{id} and /commodities")
+    public  class FetchTests {
+
+
+    }
 
 }
--- CA1/src/test/java/ApiTest/UserControllerTest.java
+++ CA1/src/test/java/ApiTest/UserControllerTest.java
@@ -2,14 +2,15 @@ package ApiTest;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import application.BalootApplication;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import controllers.UserController;
 import model.User;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -20,7 +21,9 @@ import org.springframework.web.context.WebApplicationContext;
 import service.Baloot;
 
 
+
 @SpringBootTest(classes = BalootApplication.class)
+@DisplayName("User Controller Api Tests")
 public class UserControllerTest {
 
     @Autowired
@@ -29,6 +32,9 @@ public class UserControllerTest {
 
     @Autowired
     private UserController controller;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @MockBean
     private Baloot baloot;
@@ -39,11 +45,60 @@ public class UserControllerTest {
         controller.setBaloot(baloot);
     }
 
-    @Test
-    public void exampleTest() throws Exception {
-        when(baloot.getUserById("1")).thenReturn(new User());
 
-        this.mockMvc.perform(get("/users/1")).andDo(print()).andExpect(status().isOk());
+    @Nested
+    @DisplayName("Tests For /users/{id}")
+    public class getUserTests{
+
+        static String URI = "/users/{id}";
+
+        @Test
+        void CALLING_getUser_WHEN_validUserId_THEN_returnUserWith200() throws Exception {
+
+            var id = "userId";
+            when(baloot.getUserById(id)).thenReturn(new User());
+
+            mockMvc
+                    .perform(get(URI, id))
+                    .andExpect(status().isOk())
+                    .andReturn();
+        }
+
+        @Test
+        void CALLING_getUser_WHEN_notExistentUser_THEN_returnErrorWith404(){
+            Assertions.fail("Not Implemented Yet!");
+        }
+
+
     }
+
+    @Nested
+    @DisplayName("Tests For /users/{id}/credit")
+    public class addCreditTests{
+
+        static String URI = "/users/{id}/credit";
+        @Test
+        void CALLING_addCredit_WHEN_validUserValidCredit_THEN_returnErrorWith404(){
+            Assertions.fail("Not Implemented Yet!");
+        }
+        @Test
+        void CALLING_addCredit_WHEN_notExistentUser_THEN_returnErrorWith404(){
+            Assertions.fail("Not Implemented Yet!");
+        }
+        @Test
+        void CALLING_addCredit_WHEN_notIncludingCredit_THEN_returnErrorWith400(){
+            Assertions.fail("Not Implemented Yet!");
+        }
+        @Test
+        void CALLING_addCredit_WHEN_negativeCredit_THEN_returnErrorWith400(){
+            Assertions.fail("Not Implemented Yet!");
+        }
+        @Test
+        void CALLING_addCredit_WHEN_textAsCredit_THEN_returnErrorWith400(){
+            Assertions.fail("Not Implemented Yet!");
+        }
+
+    }
+
 
 }
